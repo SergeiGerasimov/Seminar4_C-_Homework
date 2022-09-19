c8af1bc452a5bd691d18c16215d4094a2324f0cc
SergeiGerasimov <110515673+SergeiGerasimov@users.noreply.github.com>
2022-09-19 08:46:22 +0700

    Task0

---
+++ Task0/Program.cs
@@ -0,0 +1,18 @@
+﻿// Написать программу, которая принимает на вход число (А) 
+// и выдает сумму чисел от 1 до А
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+Console.WriteLine("Введите целое положительное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int sum = 0;
+
+for (int i = 1; i <= num; i++)
+{
+    sum = sum + i;
+    
+}
+Console.WriteLine($"Сумма чисел от 1 до {num} = {sum}");
+
