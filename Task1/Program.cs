b8a73db621e819382ccceeb93386d161f9cc0bc1
SergeiGerasimov <110515673+SergeiGerasimov@users.noreply.github.com>
2022-09-20 12:30:35 +0700

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход число 
+// и выдает количество цифр в числе
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int count = 0;
+int NumberofDigit(int num)
+{
+    while (num > 0)
+    {
+        count++;
+        num = num / 10;
+    }
+    return count;
+}
+
+Console.WriteLine($"Количество чисел: {NumberofDigit(number)}");
