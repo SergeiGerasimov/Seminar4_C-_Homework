33df8a77837a421cac072697e878270536038426
SergeiGerasimov <110515673+SergeiGerasimov@users.noreply.github.com>
2022-09-20 21:21:06 +0700

    Task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -2,3 +2,19 @@
 // и выдает произведение чисел от 1 до N
 // 4 -> 24
 // 5 -> 120
+
+Console.WriteLine("Введите целое положительное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int Factorial(int number)
+{
+    int result = 1;
+    for (int i = 1; i <= number; i++)
+    {
+        result = result * i;
+    }
+    return result;
+}
+
+int result = Factorial(num);
+Console.WriteLine($"Сумма чисел от 1 до {num} = {result}");
